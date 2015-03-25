c4c9edc797a26a2c07e4ae6ba3e82bf286abbbb6
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2015-03-25 16:58:37 +0000

    03 - Encripting and Printing Text.

--- src/gmit/PolybiusSquare.java
+++ src/gmit/PolybiusSquare.java
@@ -1,5 +1,55 @@
 package gmit;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PolybiusSquare {
+	Map<String, String> polybius = new HashMap<String, String>();
 	
+	public PolybiusSquare(){
+		
+		polybius.put("A", "DG");
+		polybius.put("B", "VX");
+		polybius.put("C", "GV");
+		polybius.put("D", "FX");
+		polybius.put("E", "DF");
+		polybius.put("F", "FV");
+		polybius.put("G", "AV");
+		polybius.put("H", "AD");
+		polybius.put("I", "XV");
+		polybius.put("J", "XA");
+		polybius.put("K", "GD");
+		polybius.put("L", "FA");
+		polybius.put("M", "DD");
+		polybius.put("N", "FF");
+		polybius.put("O", "FG");
+		polybius.put("P", "AA");
+		polybius.put("Q", "AG");
+		polybius.put("R", "GF");
+		polybius.put("S", "VA");
+		polybius.put("T", "XG");
+		polybius.put("U", "XF");
+		polybius.put("V", "GX");
+		polybius.put("W", "VG");
+		polybius.put("X", "GA");
+		polybius.put("Y", "DX");
+		polybius.put("Z", "VF");
+		polybius.put("0", "AF");
+		polybius.put("1", "DV");
+		polybius.put("2", "FD");
+		polybius.put("3", "GG");
+		polybius.put("4", "DA");
+		polybius.put("5", "VD");
+		polybius.put("6", "AX");
+		polybius.put("7", "VV");
+		polybius.put("8", "XX");
+		polybius.put("9", "XD");
+	}
+	
+	public String encript(String key){
+		String val;
+		val = polybius.get(key);
+		
+		return val;
+	}
 } // class
--- src/gmit/TestRunner.java
+++ src/gmit/TestRunner.java
@@ -9,13 +9,30 @@ import java.io.InputStreamReader;
 public class TestRunner {
 	public static void main(String[] args) throws IOException {
 
+		PolybiusSquare p = new PolybiusSquare();
 
-		File f = new File("WarAndPeace-LeoTolstoy.txt");
+		File f = new File("PoblachtNaHEireann.txt");
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
 		
+		// Printing text from the file
 		String line = null;
 		while((line = br.readLine()) != null){
 			System.out.println(line);
-		}
+			
+			String[] words = line.split(" ");
+			for(int i = 0; i < words.length; i++){
+				String word = words[i];
+				for(int j = 0; j < word.length(); j++){
+					String letter = String.format("%c", word.charAt(j));
+					System.out.print(p.encript(letter.toUpperCase().trim()));
+				} // end nested for loop
+			} // end for
+			System.out.println("\n");
+		} // while 
+		
+		br.close();
+		
+		
+		
 	} // main
 } // class
