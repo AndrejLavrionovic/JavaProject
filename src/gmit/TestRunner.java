ddea76a5abde06d53dca6c0ec501bc38483f886b
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2015-03-25 13:50:19 +0000

    02 - Addad files.txt to the project

--- src/gmit/TestRunner.java
+++ src/gmit/TestRunner.java
@@ -1,7 +1,21 @@
 package gmit;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestRunner {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+
+		File f = new File("WarAndPeace-LeoTolstoy.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
 		
+		String line = null;
+		while((line = br.readLine()) != null){
+			System.out.println(line);
+		}
 	} // main
 } // class
