db2f7906b7d1b307f96f8594edd000e97128e0d6
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2015-03-31 18:50:53 +0100

    10 - Encripting and Decripting text

--- src/gmit/PolybiusSquare.java
+++ src/gmit/PolybiusSquare.java
@@ -5,9 +5,22 @@ import java.util.Map;
 
 public class PolybiusSquare {
 	Map<Character, String> polybius = new HashMap<Character, String>();
+	Map<String, Character> decPol = new HashMap<String, Character>();
 	
 	public PolybiusSquare(){
-		
+		encPut();
+		decPut();
+	}
+	
+	public String encrypt(char key){
+		return polybius.get(key);
+	}
+	
+	public char decrypt(String key){
+		return decPol.get(key);
+	}
+	
+	private void encPut(){
 		polybius.put('a', "DG");
 		polybius.put('b', "VX");
 		polybius.put('c', "GV");
@@ -64,10 +77,60 @@ public class PolybiusSquare {
 		polybius.put('/', "YV");
 	}
 	
-	public String encrypt(char key){
-		String val;
-		val = polybius.get(key);
-		
-		return val;
+	private void decPut(){
+		decPol.put("DG", 'a');
+		decPol.put("VX", 'b');
+		decPol.put("GV", 'c');
+		decPol.put("FX", 'd');
+		decPol.put("DF", 'e');
+		decPol.put("FV", 'f');
+		decPol.put("AV", 'g');
+		decPol.put("AD", 'h');
+		decPol.put("XV", 'i');
+		decPol.put("XA", 'j');
+		decPol.put("GD", 'k');
+		decPol.put("FA", 'l');
+		decPol.put("DD", 'm');
+		decPol.put("FF", 'n');
+		decPol.put("FG", 'o');
+		decPol.put("AA", 'p');
+		decPol.put("AG", 'q');
+		decPol.put("GF", 'r');
+		decPol.put("VA", 's');
+		decPol.put("XG", 't');
+		decPol.put("XF", 'u');
+		decPol.put("GX", 'v');
+		decPol.put("VG", 'w');
+		decPol.put("GA", 'x');
+		decPol.put("DX", 'y');
+		decPol.put("VF", 'z');
+		decPol.put("AF", '0');
+		decPol.put("DV", '1');
+		decPol.put("FD", '2');
+		decPol.put("GG", '3');
+		decPol.put("DA", '4');
+		decPol.put("VD", '5');
+		decPol.put("AX", '6');
+		decPol.put("VV", '7');
+		decPol.put("XX", '8');
+		decPol.put("XD", '9');
+		decPol.put("AZ", '.');
+		decPol.put("DZ", ',');
+		decPol.put("FZ", '\"');
+		decPol.put("GZ", ':');
+		decPol.put("VZ", '-');
+		decPol.put("XZ", '?');
+		decPol.put("ZA", '(');
+		decPol.put("ZD", ')');
+		decPol.put("ZF", ' ');
+		decPol.put("ZG", '_');
+		decPol.put("ZV", ';');
+		decPol.put("ZX", '\'');
+		decPol.put("ZZ", '!');
+		decPol.put("YD", '[');
+		decPol.put("YF", ']');
+		decPol.put("YG", '=');
+		decPol.put("YV", '/');
+		decPol.put("YA", '*');
 	}
 } // class
--- src/gmit/TestRunner.java
+++ src/gmit/TestRunner.java
@@ -19,14 +19,11 @@ public class TestRunner {
 
 		// VARIABLES
 		//*****************************************************
-		String keyWord = "ELEPHANABT", codeLine = null, keyWordSorted;
+		String keyWord = "JEVAX", codeLine = null, keyWordSorted;
 		int i, j, k, index = 0, element, pos;
 		char c;
 		int[] indexOrder = new int[keyWord.length()];
 		char[] keyWordArr = new char[keyWord.length()];
-		
-		List<ArrayList> l = new ArrayList<ArrayList>();
-		List<Character> listColumn = new ArrayList<Character>();
 		
 		PolybiusSquare p = new PolybiusSquare();
 		
@@ -45,13 +42,13 @@ public class TestRunner {
 			} // end nested for-loop
 			keyWordArr[index] = (char)127;
 			indexOrder[i] = index;
-			//System.out.println(indexOrder[i]);
+			System.out.println(indexOrder[i]);
 		} // end outer for-loop
 		//******************************************************************************
 		//******************************************************************************
-		System.out.println("\n\n");
+		//System.out.println("KeyWord length is: "+keyWord.length());
 		
-		
+
 		//******************************************************************************
 		// Encoding
 		//******************************************************************************
@@ -59,16 +56,19 @@ public class TestRunner {
 		//File fOut = new File("PoblachtNaHEireann.txt");
 		//File fOut = new File("DeBelloGallico.txt");
 		File fOut = new File("WarAndPeace-LeoTolstoy.txt");
+		//File fOut = new File("text.txt");
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fOut)));
 		
 		FileWriter encFIn = new FileWriter("enc.txt");
 		
 		// Printing text from the file
 		String line = null;
-		int ArrayListIndex = 0;
+
+		//int ArrayListIndex = 1;
 		while((line = br.readLine()) != null){
 			//System.out.println(line);
 			
+			/*
 			//*************************************************************************
 			// Removing spaces
 			//*************************************************************************
@@ -78,26 +78,72 @@ public class TestRunner {
 				line += words[i].trim();
 			}
 			//System.out.println(line);
+			//encFIn.append(line+"\n");
+			//System.out.println("Line length is: " + line.length() + "\n");
+			 * 
+			 */
 			
 			//**************************************************************************
 			// Encoding and Encrypting line by line
 			//**************************************************************************
 			for(i = 0; i < keyWord.length(); i++){
 				index = indexOrder[i];
+				int row = 0; // setting row to zero before every new column
 				for(j = index / 2; j < line.length(); j += keyWord.length() / 2){
 					char letter = line.charAt(j);
 					if(65 <= (int)letter && (int)letter <= 90){
 						letter = Character.toLowerCase(letter);
 					}
-					if((index % 2) == 0){
-						c = p.encrypt(letter).charAt(0);
+					/*
+					System.out.println(ArrayListIndex + ")");
+					System.out.println("j = " + index + " / 2 = " + index / 2);
+					System.out.println("letter is " + letter);
+					System.out.println("index = " + index + "; j = " + j + "; index % 2 = " + index % 2 + "; row = " + (row + 1));
+					*/
+					if(keyWord.length() % 2 == 0){
+						if((index % 2) == 0){
+							c = p.encrypt(letter).charAt(0);
+							//System.out.println("First symbol of " + letter + " character is " + c);
+						}
+						else{
+							c = p.encrypt(letter).charAt(1);
+							//System.out.println("Second symbol of " + letter + " character is " + c);
+						}
 					}
+					
 					else{
-						c = p.encrypt(letter).charAt(1);
-					}
+						if(row % 2 == 0){
+							if((index % 2) == 0){
+								c = p.encrypt(letter).charAt(0);
+								//System.out.println("First symbol of " + letter + " character is " + c);
+								
+							}
+							else{
+								c = p.encrypt(letter).charAt(1);
+								//System.out.println("Second symbol of " + letter + " character is " + c);
+								j++;
+							}
+						} // if row 1, 3, 5 and so on
+						else{
+							if((index % 2) == 0){
+								c = p.encrypt(letter).charAt(1);
+								//System.out.println("Second symbol of " + letter + " character is " + c);
+								j++;
+							}
+							else{
+								c = p.encrypt(letter).charAt(0);
+								//System.out.println("First symbol of " + letter + " character is " + c);
+							}
+							
+						} // else if row 2, 4, 6 and so on
+						
+						
+						
+						row++;
+					} // end else if keyWord % 2 is NOT 0
 					//System.out.print(listColumn.get(ArrayListIndex));
 					encFIn.append(c);
-					ArrayListIndex++;
+					//ArrayListIndex++;
 				}
 			}
 			encFIn.append('\n');
@@ -105,8 +151,14 @@ public class TestRunner {
 			//**************************************************************************
 			
 			 //System.out.println();
-			 
-			/*
+			
+			
+/*			
+			//**************************************************************************
+			// Bad code
+			//**************************************************************************
+			//**********************************************************************
+			//**********************************************************************
 			codeLine = "";
 			
 			String[] words = line.split(" ");
@@ -140,21 +192,89 @@ public class TestRunner {
 			} // end outer for loop
 			
 			System.out.println("\n");
-			*/
+			//**********************************************************************
+			//**********************************************************************
+			//**********************************************************************
+			//**********************************************************************
+*/			
 		} // while 
 		br.close();
 		encFIn.close();
 		
+
+		//**************************************************************************
+		// Decrypting
+		//**************************************************************************
+		File encFile = new File("enc.txt");
+		BufferedReader brDec = new BufferedReader(new InputStreamReader(new FileInputStream(encFile)));
+		FileWriter decFOut = new FileWriter("dec.txt");
 		
+		line = null;
+		while((line = brDec.readLine()) != null){
+			// Searching for characters and
+			// building the string of two
+			// characters for encoding. Than
+			// again and again until the line of text
+			// is build.
+
+			String cipher = "";
+			int rows = line.length() / keyWord.length(), loops, jLen = keyWord.length();
+			int reminder = line.length() % keyWord.length();
+			
+			if(reminder != 0) loops = (line.length() / keyWord.length()) + 1;
+			else loops = rows;
+			
+			for(i = 0; i < loops; i++){
+				// looking for position of character
+				// that suppose to be first in the
+				// particular column. In other word looking for
+				// first column than second and so on.
+				//System.out.print("\n" + (i+1) + ") ");
+				
+				if(reminder != 0 ){
+					if((i + 1) == loops){
+						jLen = reminder;
+					}
+				}
+				
+				for(j = 0; j < jLen; j++){
+					
+					// 1) Calculating position of index j
+					int colLength = 0;
+					for(k = 0; k < indexOrder.length; k++){
+						
+						if(indexOrder[k] == j){
+							cipher += String.format("%c", line.charAt(((rows * k) + colLength) + i));
+							break;
+						}
+						
+						if(indexOrder[k] < reminder){
+							colLength++;
+						}
+					} // end for loop searching for position of character
+
+					
+					if(cipher.length() == 2){
+						//System.out.print("String code is: " + cipher);
+						c = p.decrypt(cipher);
+						//System.out.println(" that represent letter : " + c);
+						decFOut.append(c);
+						cipher = "";
+					}
+				} // nested for (j - columns)
+			} // outer for (i - rows)
+			decFOut.append("\n");
+		 
+		} // while
 		//**************************************************************************
-		// Decryption
 		//**************************************************************************
-		//File encFile = new File("enc.txt");
-		//**************************************************************************
-		//**************************************************************************
+
+		
+		brDec.close();
+		decFOut.close();
 		
 		long endTime = System.currentTimeMillis();
-		System.out.println("\n\tTook "+(endTime - startTime) / 1000 + " seconds");
+		System.out.println("\n\tTook "+(endTime - startTime) + " miliseconds");
 		
 	} // main
 } // class
